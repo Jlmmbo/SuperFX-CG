6e35629f4a45b2678911acb57dca498c0ecfa987
fgazaille <felix.gazaille@gmail.com>
2025-11-14 12:30:35 -0400

    (untested) Temporary UI stuff

--- src/UI.h
+++ src/UI.h
@@ -181,21 +181,30 @@ Rom main_menu_ui(int keybinds[12]){
         put_disp();
         PrintXY(1, 2, "  Main menu", 0, 0);
         if (keydownlast(KEY_SHIFT_OPTN)){
+            
+            PrintXY(1,2,)
             //Settings
+            // Keybinds
+            if (keydownlast(KEY_CTRL_OPTN)){
+                keybinds[0] = map_key_ui("D-UP");
+                keybinds[1] = map_key_ui("D-DOWN");
+                keybinds[2] = map_key_ui("D-LEFT");
+                keybinds[3] = map_key_ui("D-RIGHT");
+                keybinds[4] = map_key_ui("A");
+                keybinds[5] = map_key_ui("B");
+                keybinds[6] = map_key_ui("X");
+                keybinds[7] = map_key_ui("Y");
+                keybinds[8] = map_key_ui("L");
+                keybinds[9] = map_key_ui("R");
+                keybinds[10] = map_key_ui("START");
+                keybinds[11] = map_key_ui("SELECT");
+            }
+            //  Brightness
+            //  Frameskip
+            //  colour palet
         }
-        if (keydownlast(KEY_CTRL_OPTN)){
-            keybinds[0] = map_key_ui("D-UP");
-            keybinds[1] = map_key_ui("D-DOWN");
-            keybinds[2] = map_key_ui("D-LEFT");
-            keybinds[3] = map_key_ui("D-RIGHT");
-            keybinds[4] = map_key_ui("A");
-            keybinds[5] = map_key_ui("B");
-            keybinds[6] = map_key_ui("X");
-            keybinds[7] = map_key_ui("Y");
-            keybinds[8] = map_key_ui("L");
-            keybinds[9] = map_key_ui("R");
-            keybinds[10] = map_key_ui("START");
-            keybinds[11] = map_key_ui("SELECT");
+        if (keydownlast(KEY_CTRL_EXIT)){
+            //Exits the emulator
         }
         if (keydownlast(KEY_CTRL_EXE)){
             return test_rom;
